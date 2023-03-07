c757e25c46542e7bb11041b17f12e348b4e4823c
Ignacio Gonzalez Chemes <nachogonza123@gmail.com>
2023-03-07 10:18:20 -0300

    Modifico envfilepathconfig

--- src/Configs/EnvFilePathConfig.ts
+++ src/Configs/EnvFilePathConfig.ts
@@ -1,9 +1,9 @@
 import EnumEnv from './EnumEnv';
 
 export const envFilePathConfiguration = (): string => {
-    console.log(`Entorno - ${process.env.MS_ADMIN}`);
+    console.log(`Entorno - ${process.env.SEED_ENV}`);
     let envFilePath;
-    switch (process.env.MS_ADMIN) {
+    switch (process.env.SEED_ENV) {
         case EnumEnv.LOCAL:
             envFilePath = '.env.local';
             break;
